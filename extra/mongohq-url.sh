ae71161c6d434a6ea7086e5ea27b381616ad1cde
PB <pbooth80@gmail.com>
2014-11-10 15:15:49 +0000

    Update mongohq-url.sh

--- extra/mongohq-url.sh
+++ extra/mongohq-url.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # Add an alias of MONGOHQ_URL to MONGO_URL.
-echo "-----> Adding MONGOHQ_URL -> MONGO_URL env"
+echo "-----> Adding MONGO_URL env"
 cat > "$APP_CHECKOUT_DIR"/.profile.d/mongo.sh <<EOF
   #!/bin/sh
-  export MONGO_URL=\${MONGO_URL:-\$MONGOHQ_URL}
+  export MONGO_URL=\${MONGO_URL:-`ruby -e "require 'json'; puts JSON.parse(ENV['VCAP_SERVICES']).find { |key,value| key.include? 'mongodb' }[1][0]['credentials']['url']"`}}
 EOF
 
