24019a56811f055b8cc425e14039cb8702488dc5
PB <pbooth80@gmail.com>
2014-11-10 16:24:55 +0000

    Update mongohq-url.sh

--- extra/mongohq-url.sh
+++ extra/mongohq-url.sh
@@ -6,8 +6,5 @@ cat > "$APP_CHECKOUT_DIR"/.profile.d/mongo.sh <<EOF
   export ROOT_URL=\${ROOT_URL:-`ruby -e 'require "json"; puts "http://#{JSON.parse(ENV["VCAP_APPLICATION"])["application_uris"][0]}"'`}
   export PORT=\${PORT:-$VCAP_APP_PORT}
   export MONGO_URL=\${MONGO_URL:-`ruby -e "require 'json'; puts JSON.parse(ENV['VCAP_SERVICES']).find { |key,value| key.include? 'mongodb' }[1][0]['credentials']['url']"`}
-  echo $ROOT_URL
-  echo $PORT
-  echo $MONGO_URL
 EOF
 
